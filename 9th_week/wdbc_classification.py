92b6dc46f7773670f8d4014c5032a6fbd651239a
minn232 <mjlee320@naver.com>
2024-11-06 14:29:04 +0900

    9th_week assignment

--- 9th_week/wdbc_classification.py
+++ 9th_week/wdbc_classification.py
@@ -1,7 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import (datasets, svm, metrics)
-from matplotlib.lines import Line2D # For the custom legend
+from sklearn import (datasets, metrics, svm, tree)
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import ConfusionMatrixDisplay
+from matplotlib.lines import Line2D  # For the custom legend
+
+from sklearn.model_selection import train_test_split
 
 def load_wdbc_data(filename):
     class WDBCData:
@@ -15,17 +19,23 @@ def load_wdbc_data(filename):
     with open(filename) as f:
         for line in f.readlines():
             items = line.split(',')
-            wdbc.target.append(items[1])        # TODO #1) Add the true label (0 for M / 1 for others)
-            wdbc.data.append(items[2:])         # TODO #1) Add 30 attributes (as floating-point numbers)
-        wdbc.data = np.array(wdbc.data)
+            # TODO #1) Add the true label (0 for M / 1 for others)
+            wdbc.target.append(0 if items[1] == 'M' else 1)
+            # TODO #1) Add 30 attributes (as floating-point numbers)
+            wdbc.data.append([float(x) for x in items[2:]])
+    
+    wdbc.data = np.array(wdbc.data)
+    wdbc.target = np.array(wdbc.target)
     return wdbc
 
 if __name__ == '__main__':
     # Load a dataset
-    wdbc = load_wdbc_data('data/wdbc.data')     # TODO #1) Implement 'load_wdbc_data()'
+    # TODO #1) Implement 'load_wdbc_data()'
+    wdbc = load_wdbc_data('./data/wdbc.data')     
 
     # Train a model
-    model = svm.SVC()                           # TODO #2) Find a better classifier (SVC accuracy: 0.902)
+    # TODO #2) Find a better classifier (SVC accuracy: 0.902)
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(wdbc.data, wdbc.target)
 
     # Test the model
@@ -33,6 +43,11 @@ if __name__ == '__main__':
     accuracy = metrics.balanced_accuracy_score(wdbc.target, predict)
 
     # TODO #3) Visualize the confusion matrix
+    conf_matrix = metrics.confusion_matrix(wdbc.target, predict)
+    ConfusionMatrixDisplay(conf_matrix, display_labels=wdbc.target_names).plot()
+    plt.title("Confusion Matrix")
+    plt.savefig("wdbc_classification_matrix.png")  # Save the matrix image
+    plt.show()
 
     # Visualize testing results
     cmap = np.array([(1, 0, 0), (0, 1, 0)])
@@ -44,4 +59,5 @@ if __name__ == '__main__':
         plt.xlabel(wdbc.feature_names[x])
         plt.ylabel(wdbc.feature_names[y])
         plt.legend(handles=clabel, framealpha=0.5)
+    plt.savefig("wdbc_classification_scatter.png")  # Save the matrix image
     plt.show()
